d24969960578b137311085af19b6325fdc162674
Ekrem <avekremyilmazturk@gmail.com>
2023-11-07 20:13:47 +0300

    up

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 "use strict";
-/* -------------------------------------------------------
-    EXPRESSJS - TODO Project with Sequelize
-------------------------------------------------------- */
+/* -------------------------------------------------------*/
 
 const express = require("express");
 const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+
+
 /* ------------------------------------------------------- */
 //TEMPLATE  (npm i ejs)
 app.set('view engine', 'ejs')                               //---> view engine olarak 3 seçeneğim var ejs kullanacaksın diyorum
-
+// Default folder: './views'
+app.set('views', './public')                                //---> çalışacağım dosyayı public içnde ara dedim
 
 
 /* ------------------------------------------------------- */
@@ -19,10 +20,11 @@ app.set('view engine', 'ejs')                               //---> view engine o
 app.use(express.json())
 
 // Router:
-app.all('/', (req, res) => {                               //---> anasayfayı template e devrettim
+app.all('/', (req, res) => {                                //---> anasayfayı template e devrettim
     res.render('index.ejs' )                                //---> ilk string hangi ejs dosyasını çalıştırcaksam o 
 })
 
+app.use('/view', require('./app/routes/todoTemplate'))  
 app.use('/api', require('./app/routes/todo'))               //---> api ile ilgili yapacağım ne varsa route'u yönlendirdim
 
 
