d66ad03d857965a700523adeee247ebd4f59a73d
Ekrem <avekremyilmazturk@gmail.com>
2023-11-08 16:08:06 +0300

    uuuppp

--- app/errorHandler.js
+++ app/errorHandler.js
@@ -3,12 +3,26 @@
 
 module.exports = (err, req, res, next) => {
     const errorStatusCode = res.errorStatusCode ?? 500
-    console.log('errorHandler runned.')
-    res.status(errorStatusCode).send({
-        error: true, // special data
-        message: err.message, // error string message
-        cause: err.cause, // error option cause
-        // stack: err.stack, // error details
-        body: req.body,
-    })
+
+    const data ={
+        
+            error: true, // special data
+            message: err.message, // error string message
+            cause: err.cause, // error option cause
+            // stack: err.stack, // error details
+            body: req.body
+    
+    }
+
+    // console.log('errorHandler ran.')
+
+    if (req.url.startsWith('/api')) {
+
+        res.status(errorStatusCode).send(data)
+        
+    } else {
+
+        res.render('error', { data })
+    }
+    
 }
