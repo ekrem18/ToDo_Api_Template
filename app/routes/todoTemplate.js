f0d2df6a594007a2747e34d079f83428c723a6ae
Ekrem <avekremyilmazturk@gmail.com>
2023-11-09 12:15:58 +0300

    up

--- app/routes/todoTemplate.js
+++ app/routes/todoTemplate.js
@@ -5,26 +5,25 @@ const router = require('express').Router()
 // Call TODO Controller:
 const todoTemplate = require('../controllers/todoTemplate')
 
-// router.route('/')
-//     .get(todoTemplate.list) // LIST
-//     .post(todoTemplate.create) // CREATE
 
-// router.route('/:id')
-//     .get(todoTemplate.read) // READ
-//     .put(todoTemplate.update) // UPDATE
-//     .delete(todoTemplate.delete) // DELETE
 
-router.get('/', todoTemplate.list)
+// router.get('/', todoTemplate.list)
 
-router.get('/create', todoTemplate.create)              
-router.post('/create', todoTemplate.create)                 //---> Form verilerini doldurup create butonuna bastıımda çalışacak olan route
+// router.get('/create', todoTemplate.create)              
+// router.post('/create', todoTemplate.create)                 //---> Form verilerini doldurup create butonuna bastıımda çalışacak olan route
 
-router.get('/:id', todoTemplate.read)
+// router.get('/:id', todoTemplate.read)
 
-router.get('/:id/update', todoTemplate.update)              //---> Form görüntüleme 
-router.post('/:id/update', todoTemplate.update)             //---> Formu işleme
+// router.get('/:id/update', todoTemplate.update)              //---> Form görüntüleme 
+// router.post('/:id/update', todoTemplate.update)             //---> Formu işleme
 
-router.get('/:id/delete', todoTemplate.delete)
+// router.get('/:id/delete', todoTemplate.delete)
+
+router.all('/', todoTemplate.list)
+router.all('/create', todoTemplate.create)
+router.all('/:id', todoTemplate.read)
+router.all('/:id/update', todoTemplate.update)
+router.all('/:id/delete', todoTemplate.delete)
 
 
 
